169bd336e785af6f4ee43678de3b75838bc16ed1
tipeJ <tiitus.jarvinen@gmail.com>
2019-09-29 02:00:49 +0300

    Added some extra settings options

--- lib/UI/Preferences.dart
+++ lib/UI/Preferences.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:lyre/Resources/globals.dart';
 import 'package:lyre/UI/CustomExpansionTile.dart';
 import '../Themes/themes.dart';
 import '../Themes/bloc/theme_bloc.dart';
 import '../Themes/bloc/theme_event.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import '../Resources/PreferenceValues.dart';
 
-class PreferencesView extends StatelessWidget {
+class PreferencesView extends StatefulWidget {
+  PreferencesView({Key key}) : super(key: key);
+
+  _PreferencesViewState createState() => _PreferencesViewState();
+}
+class _PreferencesViewState extends State<PreferencesView> {
   SharedPreferences preferences;
 
   @override
@@ -23,13 +30,17 @@ class PreferencesView extends StatelessWidget {
                 return ListView(
                   children: <Widget>[
                     CustomExpansionTile(
-                      title: 'Themes',
-                      children: getThemeList(context),
+                      title: 'Browsing',
+                      children: getBrowsingSettings(context),
                     ),
                     CustomExpansionTile(
                       title: 'Filters',
-                      children: getFiltersList(context),
-                    )
+                      children: getFiltersSettings(context),
+                    ),
+                    CustomExpansionTile(
+                      title: 'Themes',
+                      children: getThemeSettings(context),
+                    ),
                   ],
                 );
               } else {
@@ -44,19 +55,68 @@ class PreferencesView extends StatelessWidget {
       )
     ));
   }
-  List<Widget> getFiltersList(BuildContext context){
+  List<Widget> getBrowsingSettings(BuildContext context){
     return [
-      getSettingsTitleRow(
-        'Show NSFW Previews', 
-        Switch(
-          value: preferences.getBool('showNSFWPreviews') != null ? preferences.getBool('showNSFWPreviews') : false,
+      SettingsTitleRow(
+        title: "Default sorting type",
+        leading: new DropdownButton<String>(
+          value: preferences.getString(DEFAULT_SORT_TYPE) != null ? preferences.getString(DEFAULT_SORT_TYPE) : sortTypes[0],
+          items: sortTypes.map((String value) {
+            return new DropdownMenuItem<String>(
+              value: value,
+              child: new Text(value),
+            );
+          }).toList(),
+          onChanged: (value) {
+            preferences.setString(DEFAULT_SORT_TYPE, value);
+          },
+        )
+      ),
+      StatefulBuilder(
+        builder: (BuildContext context, setState) {
+          return SettingsTitleRow(
+            title: "Default sorting time",
+            leading: new DropdownButton<String>(
+              value: preferences.getString(DEFAULT_SORT_TIME) != null ? preferences.getString(DEFAULT_SORT_TIME) : sortTimes[1],
+              items: sortTimes.map((String value) {
+                return new DropdownMenuItem<String>(
+                  value: value,
+                  child: new Text(value),
+                );
+              }).toList(),
+              onChanged: (value) {
+                preferences.setString(DEFAULT_SORT_TIME, value);
+                setState(() {
+                });
+              },
+            )
+          );
+        },
+      ),
+      
+      SettingsTitleRow(
+        title: "Reset sorting when refreshing submission list",
+        leading: Switch(
+          value: preferences.getBool(RESET_SORTING) != null ? preferences.getBool(RESET_SORTING) : true,
           onChanged: (value){
-            preferences.setBool('showNSFWPreviews', value);
+            preferences.setBool(RESET_SORTING, value);
+          },)
+      ),
+    ];
+  }
+  List<Widget> getFiltersSettings(BuildContext context){
+    return [
+      SettingsTitleRow(
+        title: 'Show NSFW Previews', 
+        leading: Switch(
+          value: preferences.getBool(SHOW_NSFW_PREVIEWS) != null ? preferences.getBool(SHOW_NSFW_PREVIEWS) : false,
+          onChanged: (value){
+            preferences.setBool(SHOW_NSFW_PREVIEWS, value);
           },)
       )
     ];
   }
-  List<Widget> getThemeList(BuildContext context){
+  List<Widget> getThemeSettings(BuildContext context){
     List<Widget> list = [];
     LyreTheme.values.forEach((lyreAppTheme){
       list.add(Container(
@@ -64,7 +124,7 @@ class PreferencesView extends StatelessWidget {
           color: lyreThemeData[lyreAppTheme].accentColor,
           shape: BoxShape.rectangle,
           borderRadius: BorderRadius.circular(8.0),
-          border: preferences.get('currentTheme') == lyreAppTheme.toString()
+          border: preferences.get(CURRENT_THEME) == lyreAppTheme.toString()
             ? Border.all(
               color: Theme.of(context).accentColor,
               width: 3.5
@@ -80,7 +140,7 @@ class PreferencesView extends StatelessWidget {
           onTap: (){
             //Make the bloc output a new ThemeState
             SharedPreferences.getInstance().then((instance){
-              instance.setString('currentTheme', lyreAppTheme.toString());
+              instance.setString(CURRENT_THEME, lyreAppTheme.toString());
               BlocProvider.of<ThemeBloc>(context)
               .dispatch(ThemeChanged(theme: lyreAppTheme));
             });
@@ -90,8 +150,17 @@ class PreferencesView extends StatelessWidget {
     });
     return list;
   }
-  Row getSettingsTitleRow(String title, Widget leading){
+}
+class SettingsTitleRow extends StatelessWidget {
+  final String title;
+  final Widget leading;
+
+  const SettingsTitleRow({this.title, this.leading});
+
+  @override
+  Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
         Padding(
           padding: EdgeInsets.symmetric(horizontal: 5.0),

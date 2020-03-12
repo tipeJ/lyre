aa98c37f348671f5c74f5126343dcc61590fec58
tipej <tiitus.jarvinen@gmail.com>
2020-03-12 09:12:17 +0200

    changed instant view configuration

--- lib/screens/instant_view.dart
+++ lib/screens/instant_view.dart
@@ -1,10 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_html/flutter_html.dart';
-import 'package:http/http.dart';
 import 'package:lyre/Resources/reddit_api_provider.dart';
 import 'package:html/parser.dart' show parse;
-import 'package:lyre/utils/utils.dart';
-
 class InstantViewScreen extends StatefulWidget {
   final Uri initialUri;
 
@@ -18,11 +15,15 @@ class _InstantViewScreenState extends State<InstantViewScreen> with SingleTicker
   AnimationController _fadeAnimation;
   ScrollController _controller;
 
+  Future<dynamic> _bodyResponse;
+
   @override
   void initState() { 
     super.initState();
     _fadeAnimation = AnimationController(vsync: this, duration: const Duration(milliseconds: 500));
     _controller = ScrollController();
+    
+    _bodyResponse = _getBodyResponse();
   }
 
   @override
@@ -34,47 +35,50 @@ class _InstantViewScreenState extends State<InstantViewScreen> with SingleTicker
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Response>(
-      future: PostsProvider().client.get(widget.initialUri),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
-          _fadeAnimation.animateTo(1.0, curve: Curves.ease);
-          // final parsedHtml = parse(snapshot.data.body);
-          // final elements = parsedHtml.children.where((e) => e.nodeType == );
-          // final elements = parsedHtml.querySelectorAll("h1, h2, h3, h4, h5, h6, p");
-          return FadeTransition(
-            opacity: _fadeAnimation,
-            child: Scaffold(
-              body: SingleChildScrollView(
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.text_format),
+        onPressed: () {
+          setState(() {
+            
+          });
+        },
+      ),
+      body: FutureBuilder(
+        future: _bodyResponse,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
+            _fadeAnimation.animateTo(1.0, curve: Curves.ease);
+            final elements = snapshot.data.querySelectorAll("h1, h2, h3, h4, h5, h6, p");
+            return FadeTransition(
+              opacity: _fadeAnimation,
+              child: ListView.separated(
                 padding: const EdgeInsets.all(10.0),
                 controller: _controller,
-                child: Html(
-                  data: snapshot.data.body,
-                  onLinkTap: (str) => handleLinkClick(Uri.parse(str), context),
-                ),
-              )
-              // body: ListView.builder(
-              //   padding: const EdgeInsets.all(10.0),
-              //   controller: _controller,
-              //   itemCount: elements.length,
-              //   itemBuilder: (context, i) {
-              //     final e = elements[i];
-              //     return Text(
-              //       e.text,
-              //       style: _headers.contains(e.localName)
-              //         ? Theme.of(context).textTheme.title.apply(
-              //             fontSizeFactor: 1 - (_headers.indexOf(e.localName) / 10)
-              //           )
-              //         : Theme.of(context).textTheme.body1.apply(color: Theme.of(context).textTheme.body2.color)
-              //     );
-              //   }
-              // )
-            ),
-          );
-        }
-        return const SizedBox();
-      },
+                itemCount: elements.length,
+                separatorBuilder: (context, i) => _headers.contains(elements[i].localName) ? const SizedBox(height: 25.0) : const SizedBox(height: 15.0),
+                itemBuilder: (context, i) {
+                  final e = elements[i];
+                  return Text(
+                    e.text,
+                    style: _headers.contains(e.localName)
+                      ? Theme.of(context).textTheme.title.apply(
+                          fontSizeFactor: 1 - (_headers.indexOf(e.localName) / 10)
+                        )
+                      : Theme.of(context).textTheme.body1
+                  );
+                }
+              ),
+            );
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
+      )
     );
+  }
+  Future<dynamic> _getBodyResponse() async {
+    final response = await PostsProvider().client.get(widget.initialUri);
+    return compute(parse, response.body);
   }
   static const List<String> _headers = ["h1", "h2", "h3", "h4", "h5", "h6"];
 }
